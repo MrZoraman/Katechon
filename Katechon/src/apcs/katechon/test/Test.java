c9a851b00ad00710f13b6a6941beab3e7b707c5c
Zora <MrZoraman@Gmail.com>
2014-05-08 09:42:43 -0700

    Created test package for testing purposes

---
+++ Katechon/src/apcs/katechon/test/Test.java
@@ -0,0 +1,30 @@
+package apcs.katechon.test;
+
+import apcs.katechon.KatechonBase;
+
+/**
+ * This is a test class for general Katechon testsing
+ * @author Matt
+ *
+ */
+public class Test extends KatechonBase
+{
+	/**
+	 * Entry point
+	 * @param args command line arguments (unused [probably])
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("hi!");
+		
+		System.out.println("maggot");
+		
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("Because I Can");
+		}
+		
+		System.out.println("abba jeezles");
+		//COMMENTS ARE AMAZING!!!!
+	}
+}
