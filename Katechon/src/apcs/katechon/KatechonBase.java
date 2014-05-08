5a88d0830f65553f806cb7ac3063459e96d07053
sn283279 <sn283279@KRVB013-93519D.kent.k12.wa.us>
2014-05-08 09:27:25 -0700

    YEah!

---
+++ Katechon/src/apcs/katechon/KatechonBase.java
@@ -0,0 +1,12 @@
+package apcs.katechon;
+
+/**
+ * The game will extend this class. It will then be plugged into some sort of game engine singleton that will then do what needs to be done to do what we want to be done.
+ * @author Matt
+ *
+ */
+//TODO: maybe a little better documentation here ^
+public abstract class KatechonBase
+{
+
+}
