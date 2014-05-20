acb602455e44adad5a63e2946645496ffc0d0e86
Floating-Imp <seanp@kallaher.org>
2014-05-19 21:32:22 -0700

    Updated the ScoreCount class and removed the Logs folder from the index.

--- Katechon/src/apcs/shoppingMaul/periodics/ScoreCount.java
+++ Katechon/src/apcs/shoppingMaul/periodics/ScoreCount.java
@@ -4,11 +4,21 @@ import apcs.katechon.periodic.IPeriodic;
 
 public class ScoreCount implements IPeriodic
 {
+	public ScoreCount()
+	{
+		this.score = 9000;
+	}
+	
+	private int score;
 
 	@Override
 	public void onTick()
 	{
-		
+		this.score--;
 	}
-
+	
+	public int getScore()
+	{
+		return score;
+	}
 }
