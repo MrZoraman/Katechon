3a769ed501c0d430c2a5b2dcc1b27182746e154a
Floating-Imp <seanp@kallaher.org>
2014-05-20 09:10:14 -0700

    Physics things

--- Katechon/src/apcs/katechon/physics/IPhysicsObject.java
+++ Katechon/src/apcs/katechon/physics/IPhysicsObject.java
@@ -1,6 +1,19 @@
 package apcs.katechon.physics;
 
+/**
+ * This should be implemented for all objects that can be acted upon by the physics engine
+ * @author Sean
+ *
+ */
 public interface IPhysicsObject
 {
+	/**
+	 * @return The magnitude at which the object is moving
+	 */
+	public int getMagnitude();
 	
+	/**
+	 * @return The direction of the object in degrees. (0 degrees being to the right)
+	 */
+	public int getDirection();
 }
