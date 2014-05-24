04fcc17b64c3c0145437a1ed6e7600af595fbb4c
Zora <MrZoraman@Gmail.com>
2014-05-24 00:45:48 -0700

    window logger now captures log entries

--- Katechon/src/apcs/katechon/KatechonEngine.java
+++ Katechon/src/apcs/katechon/KatechonEngine.java
@@ -121,6 +121,8 @@ public class KatechonEngine
 			twl.initHideKey(Keys.TILDE);
 			KWT.getInstance().addWindow(twl);
 			
+			Log.initLogger(twl);
+			
 		} catch (Exception e) {
 			System.err.println("Failed to instantiate logger!");
 			e.printStackTrace();
