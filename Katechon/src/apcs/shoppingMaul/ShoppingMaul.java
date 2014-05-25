1bc592a5b38b0fe0230608b9c8fdec9e1a3ee3d7
Zora <MrZoraman@Gmail.com>
2014-05-25 00:27:02 -0700

    we have a snow leopard on the screen!

--- Katechon/src/apcs/shoppingMaul/ShoppingMaul.java
+++ Katechon/src/apcs/shoppingMaul/ShoppingMaul.java
@@ -1,8 +1,11 @@
 package apcs.shoppingMaul;
 
 import java.awt.Color;
+
 import apcs.katechon.KatechonGameBase;
 import apcs.katechon.KatechonEngine;
+import apcs.katechon.engine.EngineManager;
+import apcs.katechon.engine.collisions.ICollidable;
 import apcs.katechon.utils.ConfigKey;
 import apcs.katechon.utils.IConfig;
 import apcs.katechon.utils.MappedConfig;
@@ -31,10 +34,15 @@ public class ShoppingMaul extends KatechonGameBase
 		
 		mainEngine.getSwingWindow().setBackgroundColor(Color.WHITE);
 		
-		SnowLeopard snowLeopard = new SnowLeopard(40, 3);
+		SnowLeopard snowLeopard = new SnowLeopard(100, 100, 10, 28, 2, true);
 		
-		mainEngine.addDrawable(snowLeopard, 0);
+		mainEngine.addDrawable(snowLeopard, 1);
 		mainEngine.scheduleTask(snowLeopard);
+//		EngineManager.getInstance().getEngine(ICollidable.class).addItem(snowLeopard);
+	
+		
+//		mainEngine.addDrawable(snowLeopard, 0);
+//		mainEngine.scheduleTask(snowLeopard);
 	}
 
 	@Override
--- Katechon/src/apcs/shoppingMaul/SnowLeopard.java
+++ Katechon/src/apcs/shoppingMaul/SnowLeopard.java
@@ -1,13 +1,29 @@
 package apcs.shoppingMaul;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import apcs.katechon.rendering.sprites.AnimatedSequence;
+import apcs.katechon.resources.SpritesheetLoader;
 import apcs.katechon.test.SimpleCollidable;
 
 public class SnowLeopard extends SimpleCollidable
 {
-
-	public SnowLeopard(int x, int y, int width, int height, int speed,boolean control)
+	public SnowLeopard(int x, int y, int width, int height, int speed, boolean control)
 	{
 		super(x, y, width, height, speed, control);
+		
+		SpritesheetLoader loader = new SpritesheetLoader(ShoppingMaul.class, "snowleopard.png", 8, 1);
+		BufferedImage[] imageFrames = loader.loadWide(0, 0, 7);
+		
+		this.frames = new AnimatedSequence<BufferedImage>(imageFrames, 2);
 	}
 	
+	private final AnimatedSequence<BufferedImage> frames;
+	
+	@Override
+	public void draw(Graphics g)
+	{
+		g.drawImage(frames.getCurrentFrame(), x, y, null);
+	}
 }
