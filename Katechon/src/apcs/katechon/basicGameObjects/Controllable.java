54ca9c10281edec1925dc2d9ec416e82aba5a053
Zora <MrZoraman@Gmail.com>
2014-05-25 23:31:23 -0700

    Moved some fundamental classes to their own package

--- Katechon/src/apcs/katechon/basicGameObjects/Controllable.java
+++ Katechon/src/apcs/katechon/basicGameObjects/Controllable.java
@@ -1,4 +1,4 @@
-package apcs.katechon;
+package apcs.katechon.basicGameObjects;
 
 import java.util.HashSet;
 import java.util.Set;
---
+++ Katechon/src/apcs/katechon/basicGameObjects/ControllableCollidable.java
@@ -0,0 +1,5 @@
+package apcs.katechon.basicGameObjects;
+
+public class ControllableCollidable {
+
+}
