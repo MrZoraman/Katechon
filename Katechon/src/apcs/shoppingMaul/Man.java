31a6d334830ef07549f6415fd3b5c5ce701d0938
Zora <MrZoraman@Gmail.com>
2014-05-26 03:06:13 -0700

    TODO: the man.

---
+++ Katechon/src/apcs/shoppingMaul/Man.java
@@ -0,0 +1,55 @@
+package apcs.shoppingMaul;
+
+import java.util.Set;
+
+import apcs.katechon.engine.collisions.Direction;
+import apcs.katechon.engine.collisions.ICollidable;
+
+public class Man implements ICollidable
+{
+	
+
+	@Override
+	public int getTopFace() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getBottomFace() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLeftFace() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRightFace() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void onCollision(Set<Direction> types)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public boolean isFinished()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
