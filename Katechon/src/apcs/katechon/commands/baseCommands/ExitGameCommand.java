0b8670eec606c81522ba753b5441834386d0195d
Zora <MrZoraman@Gmail.com>
2014-05-26 15:06:54 -0700

    Added the ability to force exit

--- Katechon/src/apcs/katechon/commands/baseCommands/ExitGameCommand.java
+++ Katechon/src/apcs/katechon/commands/baseCommands/ExitGameCommand.java
@@ -2,12 +2,36 @@ package apcs.katechon.commands.baseCommands;
 
 import apcs.katechon.KatechonEngine;
 import apcs.katechon.commands.parser.Command;
+import apcs.katechon.logging.Log;
 
 public class ExitGameCommand implements Command
 {
 	@Override
 	public void onCommand(String[] preArgs, String[] args)
 	{
-		KatechonEngine.getInstance().end();
+		if(args.length > 0)
+		{
+			if(args[0].equals("--force"))
+			{
+				System.exit(2);
+			}
+			else
+			{
+				showUsage();
+			}
+		}
+		else
+		{
+			KatechonEngine.getInstance().end();
+		}
+	}
+	
+	private void showUsage()
+	{
+		Log.chainLog()
+			.info("USAGE:")
+			.info("exit - Exits the game")
+			.info("exit --force - forces the game to exit. Data may be lost.")
+		.log();
 	}
 }
