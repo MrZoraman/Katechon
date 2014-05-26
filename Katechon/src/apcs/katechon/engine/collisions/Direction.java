83c161f0ac8bfd521022cc95ab81129c6ef35c18
Zora <MrZoraman@Gmail.com>
2014-05-26 12:55:07 -0700

    Holds radian values

--- Katechon/src/apcs/katechon/engine/collisions/Direction.java
+++ Katechon/src/apcs/katechon/engine/collisions/Direction.java
@@ -10,25 +10,44 @@ public enum Direction
 	/**
 	 * The top of the object is colliding with something
 	 */
-	TOP,
+	TOP(Math.PI / 2),
 	
 	/**
 	 * The bottom of the object is colliding with something
 	 */
-	BOTTOM,
+	BOTTOM((Math.PI * 3) / 2),
 	
 	/**
 	 * The left of the object is colliding with something
 	 */
-	LEFT,
+	LEFT(Math.PI),
 	
 	/**
 	 * The right of the object is colliding with something
 	 */
-	RIGHT,
+	RIGHT(0),
 	
 	/**
 	 * The object is not colliding with anything
 	 */
-	NONE
+	NONE(0);
+	
+	private final double radians;
+	
+	/**
+	 * Ctor
+	 * @param radians The direction represented in radians
+	 */
+	private Direction(double radians)
+	{
+		this.radians = radians;
+	}
+	
+	/**
+	 * @return The radians that this direction represents
+	 */
+	public double toRadians()
+	{
+		return radians;
+	}
 }
