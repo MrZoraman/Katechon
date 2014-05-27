fd5a7bb5953156c4d7d8bd02e2f9b942ef1b5e31
Floating-Imp <seanp@kallaher.org>
2014-05-27 09:06:02 -0700

    The ManFactory has been improved

--- Katechon/src/apcs/shoppingMaul/ManFactory.java
+++ Katechon/src/apcs/shoppingMaul/ManFactory.java
@@ -3,16 +3,20 @@ package apcs.shoppingMaul;
 import java.awt.Color;
 import java.util.Random;
 
+import apcs.shoppingMaul.colorEnums.HairColors;
+import apcs.shoppingMaul.colorEnums.HandColors;
+import apcs.shoppingMaul.colorEnums.ShoeColors;
+
 public class ManFactory
 {
 	public static Man makeMan(int x, int y, int width, int height, int speed)
 	{
 		Random r = new Random();
 		
-		Color hairColor = new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256));
+		Color hairColor = HairColors.values()[r.nextInt(HairColors.values().length - 1)].toColor();
 		Color shirtColor = new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256));
-		Color handColor = new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256));
-		Color shoeColor = new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256));
+		Color handColor = HandColors.values()[r.nextInt(HandColors.values().length - 1)].toColor();
+		Color shoeColor = ShoeColors.values()[r.nextInt(ShoeColors.values().length - 1)].toColor();
 		
 		return new Man(x, y, width, height, speed, hairColor, shirtColor, handColor, shoeColor);
 	}
