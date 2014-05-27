a546e3ac749b1073ee6703214bce63dc0681fd22
Zora <MrZoraman@Gmail.com>
2014-05-26 19:17:49 -0700

    So it begins.

---
+++ Katechon/src/apcs/shoppingMaul/Board.java
@@ -0,0 +1,17 @@
+package apcs.shoppingMaul;
+
+import apcs.katechon.basicGameObjects.ControlScheme;
+import apcs.katechon.rendering.Display;
+
+/**
+ * This is it. The game board. It keeps the player in the middle and when you move up and down it scrolls the view around to keep your character in the middle.
+ * @author Matt
+ *
+ */
+public class Board extends LeopardPack
+{
+	public Board(ControlScheme controlScheme, int speed, int amountOfLeopards, Display display, int width, int height)
+	{
+		super(controlScheme, width / 2, height / 2, speed, amountOfLeopards);
+	}
+}
