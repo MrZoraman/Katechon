bc940ed90fe1b3800d15fd5d624c6acc14635e65
Zora <MrZoraman@Gmail.com>
2014-05-27 11:02:43 -0700

    Added wall. Needs to be tested.

--- Katechon/src/apcs/shoppingMaul/map/Wall.java
+++ Katechon/src/apcs/shoppingMaul/map/Wall.java
@@ -1,12 +1,27 @@
 package apcs.shoppingMaul.map;
 
-public class Wall
+import java.awt.Color;
+import java.awt.Graphics;
+
+import apcs.shoppingMaul.SimpleBoardCollidable;
+
+public class Wall extends SimpleBoardCollidable
 {
 	private static final int WIDTH = 600;
 	private static final int HEIGHT = 600;
 	
-	public Wall(int x, int y)
-	{
-		
+	private static final int SPEED = 10;
+	
+	public Wall(int x, int y) {
+		super(x, y, WIDTH, HEIGHT, SPEED, false);
 	}
+	
+	@Override
+	public void draw(Graphics g)
+	{
+		g.setColor(Color.BLACK);
+		
+		g.fillRect(x, y, width, height);
+	}
+	
 }
