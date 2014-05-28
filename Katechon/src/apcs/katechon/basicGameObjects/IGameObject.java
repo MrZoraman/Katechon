079ecff057fdd01dd4f951a4dace7e128e808dfd
Floating-Imp <seanp@kallaher.org>
2014-05-28 09:05:22 -0700

    We don't need that anymore.

--- Katechon/src/apcs/katechon/basicGameObjects/IGameObject.java
+++
@@ -1,8 +0,0 @@
-package apcs.katechon.basicGameObjects;
-
-import apcs.katechon.engine.collisions.ICollidable;
-import apcs.katechon.rendering.IDrawable;
-
-public interface IGameObject extends ICollidable, IDrawable {
-
-}
