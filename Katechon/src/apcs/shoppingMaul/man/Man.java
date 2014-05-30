44001723777728866ca0d333a12e7e3d79ad9b49
sn283279 <sn283279@KRVB013-93519D.kent.k12.wa.us>
2014-05-30 08:55:52 -0700

    There was an attempt

--- Katechon/src/apcs/shoppingMaul/man/Man.java
+++ Katechon/src/apcs/shoppingMaul/man/Man.java
@@ -2,6 +2,7 @@ package apcs.shoppingMaul.man;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.util.HashSet;
 import java.util.Random;
